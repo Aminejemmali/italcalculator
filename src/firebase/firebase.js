db5794570420fcc2dc4eecb6952295783751069e
Aminejemmali <jemmaliamine2000@gmail.com>
2025-03-07 23:26:54 +0100

    v1

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxGuICY92vn7VystQighWpO4zrY2BSchc",
-  authDomain: "italabrasivi.firebaseapp.com",
-  projectId: "italabrasivi",
-  storageBucket: "italabrasivi.firebasestorage.app",
-  messagingSenderId: "110626431177",
-  appId: "1:110626431177:web:b2bcf5ce15596d826fad18"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
